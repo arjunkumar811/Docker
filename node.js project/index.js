a5658fa42b216535a7c058560e297763fee31251
arjunkumar811 <kumararjun26401@gmail.com>
2025-07-18 14:01:45 +0530

    Added Dockerfile

--- node.js project/index.js
+++ node.js project/index.js
@@ -2,9 +2,8 @@ const express = require("express");
 
 const app = express();
 
-app.get("/", (res, req) => {
-    res.send("Hi There")
-})
-
+app.get("/", (req, res) => {
+  res.send("Hi There");
+});
 
 app.listen(3000);
